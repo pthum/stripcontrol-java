369e85226d5efc0aa4a7776bdb71d14c28781b5e
Patrick Thum <4435508+pthum@users.noreply.github.com>
2019-07-28 13:48:59 +0200

    add swagger and ui and therefore downgrade spring to 2.1.6.RELEASE

--- stripcontrol-javabackend/src/test/java/de/backenddev/led/stripcontrol/javastripbackend/JavastripBackendApplicationTests.java
+++ stripcontrol-javabackend/src/test/java/de/backenddev/led/stripcontrol/javastripbackend/JavastripBackendApplicationTests.java
@@ -1,14 +1,17 @@
 package de.backenddev.led.stripcontrol.javastripbackend;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith ( SpringRunner.class )
 @SpringBootTest
-class JavastripBackendApplicationTests
+public class JavastripBackendApplicationTests
 {
 
 	@Test
-	void contextLoads( )
+	public void contextLoads( )
 	{
 	}
 
