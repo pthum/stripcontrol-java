b937ff71cadb78c1604a8b00b49e847a5421498f
Patrick Thum <4435508+pthum@users.noreply.github.com>
2020-04-29 23:08:14 +0200

    adapt test

--- stripcontrol-quarkusbackend/src/test/java/de/backenddev/led/stripcontrol/ExampleResourceTest.java
+++ stripcontrol-quarkusbackend/src/test/java/de/backenddev/led/stripcontrol/ExampleResourceTest.java
@@ -1,21 +1,19 @@
 package de.backenddev.led.stripcontrol;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static io.restassured.RestAssured.given;
+
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class ExampleResourceTest
+{
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
+	@Test
+	public void testHelloEndpoint( )
+	{
+		given( ).when( ).get( "/api/ledstrip" ).then( ).statusCode( 200 );
+	}
 
 }
