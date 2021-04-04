da2da97f671fd6027cece3640dc7ae8e10f19ccb
Patrick Thum <4435508+pthum@users.noreply.github.com>
2021-04-04 17:15:37 +0200

    customize css providing a dark ui

--- stripcontrol-frontend/src/App.vue
+++ stripcontrol-frontend/src/App.vue
@@ -23,7 +23,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #d6d6d6;
   margin-top: 10px;
 }
 </style>
